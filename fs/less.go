ea379462b4e22f45ccb6b85f396280e14a44599c
Felix Geisendörfer <felix@debuggable.com>
2013-01-10 22:36:57 +0400

    Readdir for less files

--- fs/less.go
+++ fs/less.go
@@ -2,8 +2,10 @@ package fs
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"os/exec"
 	"path/filepath"
 )
@@ -16,11 +18,45 @@ func newLessProcessor(baseFs http.FileSystem) http.FileSystem {
 	return &lessProcessor{baseFs: baseFs}
 }
 
+type lessFile struct {
+	http.File
+}
+
+type lessStat struct {
+	os.FileInfo
+	name string
+}
+
+func (stat *lessStat) Name() string  {
+	return stat.name
+}
+
+func (file *lessFile) Readdir(count int) ([]os.FileInfo, error) {
+	stats, err := file.File.Readdir(count)
+	if err != nil {
+		return stats, err
+	}
+
+	for i, stat := range stats {
+		name := stat.Name()
+		ext := filepath.Ext(name)
+		if ext == ".less" {
+			name = name[0:len(name)-len(ext)] + ".css"
+			stats[i] = &lessStat{FileInfo: stat, name: name}
+		}
+	}
+	return stats, nil
+}
+
 func (less *lessProcessor) Open(path string) (http.File, error) {
 	ext := filepath.Ext(path)
+	if ext == ".less" {
+		return nil, fmt.Errorf("file not found: %s", path)
+	}
+
 	file, err := less.baseFs.Open(path)
-	if err == nil || ext != ".css" {
-		return file, err
+	if ext != ".css" {
+		return &lessFile{File: file}, err
 	}
 
 	lessPath := path[0:len(path)-len(ext)] + ".less"
