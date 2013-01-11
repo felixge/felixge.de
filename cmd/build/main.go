7b62c046706eb5ed7ab324078946e7d678270573
Felix Geisendörfer <felix@debuggable.com>
2013-01-11 11:45:35 +0400

    Initial build script

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/felixge/felixge.de/fs"
+	"io"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
 func main() {
-	fs := fs.New()
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 
-	if err := list(fs, "/"); err != nil {
+	outputDir := cwd + "/build"
+
+	fs := fs.New()
+	if err := list(fs, "/", outputDir); err != nil {
 		panic(err)
 	}
 }
 
-func list(fs http.FileSystem, path string) error {
+func list(fs http.FileSystem, path string, outputDir string) error {
 	file, err := fs.Open(path)
 	if err != nil {
 		return err
@@ -28,7 +36,21 @@ func list(fs http.FileSystem, path string) error {
 
 	if !stat.IsDir() {
 		fmt.Printf("file\t%s\t%d\n", path, stat.Size())
+		outputFile, err := os.OpenFile(outputDir +path, os.O_CREATE | os.O_TRUNC | os.O_WRONLY, 0666)
+		if err != nil {
+			return err
+		}
+		defer outputFile.Close()
+
+		if _, err := io.Copy(outputFile, file); err != nil {
+			return err
+		}
+
 		return nil
+	}
+
+	if err := os.MkdirAll(outputDir + path, 0777); err != nil {
+		return err
 	}
 
 	fmt.Printf("dir\t%s\n", path)
@@ -39,7 +61,7 @@ func list(fs http.FileSystem, path string) error {
 	}
 
 	for _, stat = range stats {
-		if err := list(fs, filepath.Clean(path+"/"+stat.Name())); err != nil {
+		if err := list(fs, filepath.Clean(path+"/"+stat.Name()), outputDir); err != nil {
 			return err
 		}
 	}
