3accfa52e514c8b4889bc3a709c8aa9227bd6d6d
Felix Geisendörfer <felix@debuggable.com>
2013-01-23 20:47:57 +0100

    Remove cruft

--- fs/fs.go
+++ fs/fs.go
@@ -73,3 +73,14 @@ func New() http.FileSystem {
 
 	return fs.SubFs("/public")
 }
+
+type talk struct {
+	Title    string
+	Location string
+	Date     string
+	Url      string
+	EventUrl string
+	VideoUrl string
+	PdfUrl   string
+	CodeUrl  string
+}
